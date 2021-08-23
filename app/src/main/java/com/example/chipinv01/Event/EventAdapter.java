37eeda0d7b8b71a79822eff7b3329c5a0ce0b599
Yainele <tawtawker@gmail.com>
2021-08-23 19:26:03 +0700

    Progress bar Payment

--- app/src/main/java/com/example/chipinv01/Event/EventAdapter.java
+++ app/src/main/java/com/example/chipinv01/Event/EventAdapter.java
@@ -69,7 +69,7 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.choosenConta
         holder.bindContact(ChoosenContacts.get(position));
         holder.itemView.setId(position);
         holder.bar.setMin(0);
-        holder.bar.setMax((int) AmountForEveryUserValue);
+        holder.bar.setMax(AmountForEveryUserValue);
         holder.bar.setProgress((int) AmountForEveryUserValue);
 
     }
@@ -97,62 +97,10 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.choosenConta
             ChoosenContactName = itemView.findViewById(R.id.ChoosenContactName);
             ChoosenContactsImage= itemView.findViewById(R.id.circleImageView);
             AmountForEveryUser=itemView.findViewById(R.id.AmountForEveryUser_item);
-            //AmountForPersonalityUser= itemView.findViewById(R.id.Pesonality);
             payment = itemView.findViewById(R.id.payment);
             bar = itemView.findViewById(R.id.Bar);
             bar.getProgressDrawable().setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_IN);
-            payment.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (listener != null) {
-                        int position = getAdapterPosition();
-                        if (position != RecyclerView.NO_POSITION) {
-                            listener.OnItemClick(position);
-                            final AlertDialog.Builder builder = new AlertDialog.Builder(view.getRootView().getContext());
-                            @SuppressLint("ResourceType") View dialogView = LayoutInflater.from(view.getRootView().getContext()).inflate(R.layout.custom_dialog_for_add_amount, null);
-                            de.hdodenhof.circleimageview.CircleImageView dialogImageView = dialogView.findViewById(R.id.dialog_boxImage);
-                            builder.setView(dialogView);
-                            builder.setCancelable(true);
-                            TextView dialog_boxUsername = dialogView.findViewById(R.id.dialog_boxUserName);
-                            TextView dialog_boxPhone = dialogView.findViewById(R.id.dialog_boxUserPhone);
-                            TextView dialog_text = dialogView.findViewById(R.id.dialog_text);
-                            Button ContinueBtn = dialogView.findViewById(R.id.ContinueBtn);
-                            Uri baseUri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, ChoosenContacts.get(position).id);
-                            Uri imageUri = Uri.withAppendedPath(baseUri, ContactsContract.Contacts.Photo.CONTENT_DIRECTORY);
-                            dialog_boxUsername.setText(ChoosenContacts.get(position).name);
-                            dialog_boxPhone.setText(ChoosenContacts.get(position).number);
-                            //dialog_text.setText("Введите ссумму которую вам вернул этот участник:");
-                            dialogImageView.setImageURI(imageUri);
-                            final AlertDialog AmountDialog = builder.show();
-                            final EditText AmountInput = dialogView.findViewById(R.id.dialog_UserAmountText);
-                            Toast.makeText(view.getContext(), "Click!", Toast.LENGTH_SHORT).show();
-                            ContinueBtn.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    if(!AmountInput.getText().toString().equals(null)) {
-                                        AmountDialog.cancel();
 
-                                        //AmountForPersonalityUser.setText(AmountInput.getText().toString());
-                                        AmountForEveryUser.setText("   ");
-                                        String dif = AmountInput.getText().toString();
-                                        difVar = Integer.parseInt(dif);
-                                        newAmount = (AmountForEveryUserValue - difVar);
-                                        String amI = String.valueOf(newAmount);
-                                        AmountForEveryUser.setText(amI);
-                                        ChoosenContacts.get(position).amount = Double.parseDouble(AmountInput.getText().toString());
-                                        AmountDialog.cancel();
-                                    }
-                                    else{
-                                        AmountDialog.cancel();
-                                    }
-
-
-                                }
-                            });
-                        }
-                    }
-                }
-            });
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -182,8 +130,11 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.choosenConta
                                         AmountDialog.cancel();
 
                                         //AmountForPersonalityUser.setText(AmountInput.getText().toString());
-                                        AmountForEveryUser.setText("   ");
+                                        //AmountForEveryUser.setText("   ");
+                                        AmountForEveryUserValue = Integer.parseInt(AmountInput.getText().toString());
+                                        String afeu = AmountForEveryUser.getText().toString();
                                         AmountForEveryUser.setText(AmountInput.getText().toString());
+                                        bar.setProgress(AmountForEveryUserValue);
                                         ChoosenContacts.get(position).amount = Double.parseDouble(AmountInput.getText().toString());
                                         AmountDialog.cancel();
                                     }
@@ -195,17 +146,76 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.choosenConta
                                 }
                             });
                         }
-                        }
                     }
+                }
 
             });
+            payment.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.OnItemClick(position);
+                            final AlertDialog.Builder builder = new AlertDialog.Builder(view.getRootView().getContext());
+                            @SuppressLint("ResourceType") View dialogView = LayoutInflater.from(view.getRootView().getContext()).inflate(R.layout.custom_dialog_for_add_amount, null);
+                            de.hdodenhof.circleimageview.CircleImageView dialogImageView = dialogView.findViewById(R.id.dialog_boxImage);
+                            builder.setView(dialogView);
+                            builder.setCancelable(true);
+                            TextView dialog_boxUsername = dialogView.findViewById(R.id.dialog_boxUserName);
+                            TextView dialog_boxPhone = dialogView.findViewById(R.id.dialog_boxUserPhone);
+                            Button ContinueBtn = dialogView.findViewById(R.id.ContinueBtn);
+                            Uri baseUri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, ChoosenContacts.get(position).id);
+                            Uri imageUri = Uri.withAppendedPath(baseUri, ContactsContract.Contacts.Photo.CONTENT_DIRECTORY);
+                            dialog_boxUsername.setText(ChoosenContacts.get(position).name);
+                            dialog_boxPhone.setText(ChoosenContacts.get(position).number);
+                            dialogImageView.setImageURI(imageUri);
+                            final AlertDialog AmountDialog = builder.show();
+                            final EditText AmountInput = dialogView.findViewById(R.id.dialog_UserAmountText);
+                            Toast.makeText(view.getContext(), "Click!", Toast.LENGTH_SHORT).show();
+                            ContinueBtn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    if(!AmountInput.getText().toString().equals(null)) {
+                                        AmountDialog.cancel();
+
+                                        //AmountForPersonalityUser.setText(AmountInput.getText().toString());
+                                        //AmountForEveryUser.setText("   ");
+
+                                        String dif = AmountInput.getText().toString();
+                                        String dif2 = AmountForEveryUser.getText().toString();
+                                        int difVar2 = Integer.parseInt(dif2);
+                                        difVar = Integer.parseInt(dif);
+                                        newAmount = (difVar2 - difVar);
+                                        String amI = String.valueOf(newAmount);
+                                        //AmountForEveryUserValue = Integer.parseInt(amI);
+                                        bar.setProgress(Integer.parseInt(amI));
+                                        AmountForEveryUser.setText(amI);
+                                        ChoosenContacts.get(position).amount = Double.parseDouble(amI);
+                                        AmountDialog.cancel();
+                                    }
+                                    else{
+                                        AmountDialog.cancel();
+                                    }
+
+
+                                }
+                            });
+                        }
+                    }
+                }
+            });
+
         }
 
        public void bindContact(Data contact) {
+
             if(AmountForEveryUserValue != -1){
                 AmountForEveryUser.setText(String.valueOf(AmountForEveryUserValue));
                 //AmountForPersonalityUser.setText(" ");
             }
+
+
            ChoosenContactName.setText(contact.name);
            ChoosenContactPhone.setText(contact.number);
            Uri baseUri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, contact.id);
