5753b32d28f4d6aa82a5699c487857bdc75ca4f3
Serio <SeriousNuts@yandex.ru>
2021-08-24 00:49:49 +0700

    fix progressBars

--- app/src/main/java/com/example/chipinv01/Event/EventAdapter.java
+++ app/src/main/java/com/example/chipinv01/Event/EventAdapter.java
@@ -7,6 +7,7 @@ import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -68,9 +69,6 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.choosenConta
     public void onBindViewHolder(@NonNull final choosenContactsViewHolder holder, final int position) {
         holder.bindContact(ChoosenContacts.get(position));
         holder.itemView.setId(position);
-        holder.bar.setMin(0);
-        holder.bar.setMax(AmountForEveryUserValue);
-        holder.bar.setProgress((int) AmountForEveryUserValue);
 
     }
 
@@ -84,12 +82,9 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.choosenConta
         TextView ChoosenContactName;
         TextView AmountForEveryUser;
         ImageView ChoosenContactsImage;
-        TextView AmountForPersonalityUser;
         ImageView payment;
         ProgressBar bar;
         int difVar=0;
-
-
 
         public choosenContactsViewHolder(@NonNull View itemView,OnItemClickListener listener) {
             super(itemView);
@@ -101,18 +96,21 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.choosenConta
             bar = itemView.findViewById(R.id.Bar);
             bar.getProgressDrawable().setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_IN);
 
-            itemView.setOnClickListener(new View.OnClickListener() {
+
+            itemView.setOnClickListener(new View.OnClickListener() {//добавление суммы
                 @Override
                 public void onClick(View v) {
                     if(listener != null){
                         int position = getAdapterPosition();
                         if(position != RecyclerView.NO_POSITION ){
                             listener.OnItemClick(position);
+
                             final AlertDialog.Builder builder = new AlertDialog.Builder(v.getRootView().getContext());
                             @SuppressLint("ResourceType")View dialogView = LayoutInflater.from(v.getRootView().getContext()).inflate(R.layout.custom_dialog_for_add_amount,null);
                             de.hdodenhof.circleimageview.CircleImageView dialogImageView = dialogView.findViewById(R.id.dialog_boxImage);
                             builder.setView(dialogView);
                             builder.setCancelable(true);
+
                             TextView dialog_boxUsername = dialogView.findViewById(R.id.dialog_boxUserName);
                             TextView dialog_boxPhone = dialogView.findViewById(R.id.dialog_boxUserPhone);
                             Button ContinueBtn = dialogView.findViewById(R.id.ContinueBtn);
@@ -121,22 +119,25 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.choosenConta
                             dialog_boxUsername.setText(ChoosenContacts.get(position).name);
                             dialog_boxPhone.setText(ChoosenContacts.get(position).number);
                             dialogImageView.setImageURI(imageUri);
+
                             final AlertDialog AmountDialog = builder.show();
                             final EditText AmountInput = dialogView.findViewById(R.id.dialog_UserAmountText);
                             ContinueBtn.setOnClickListener(new View.OnClickListener() {
                                 @Override
                                 public void onClick(View v) {
-                                    if(!AmountInput.getText().toString().equals(null)) {
-                                        AmountDialog.cancel();
+                                    if(!AmountInput.getText().toString().isEmpty()){
+                                        try {
+                                            AmountForEveryUserValue = Integer.parseInt(AmountInput.getText().toString());
+                                            AmountForEveryUser.setText(AmountInput.getText().toString());
+                                            bar.setProgress(AmountForEveryUserValue);
+                                            ChoosenContacts.get(position).amount = Double.parseDouble(AmountInput.getText().toString());
 
-                                        //AmountForPersonalityUser.setText(AmountInput.getText().toString());
-                                        //AmountForEveryUser.setText("   ");
-                                        AmountForEveryUserValue = Integer.parseInt(AmountInput.getText().toString());
-                                        String afeu = AmountForEveryUser.getText().toString();
-                                        AmountForEveryUser.setText(AmountInput.getText().toString());
-                                        bar.setProgress(AmountForEveryUserValue);
-                                        ChoosenContacts.get(position).amount = Double.parseDouble(AmountInput.getText().toString());
-                                        AmountDialog.cancel();
+                                            AmountDialog.cancel();
+
+                                        }
+                                        catch (Exception e){
+                                            Log.v("Error in onclick is :  ", "", e);
+                                        }
                                     }
                                     else{
                                         AmountDialog.cancel();
@@ -150,18 +151,20 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.choosenConta
                 }
 
             });
-            payment.setOnClickListener(new View.OnClickListener() {
+            payment.setOnClickListener(new View.OnClickListener() {//отнимание суммы
                 @Override
                 public void onClick(View view) {
                     if (listener != null) {
                         int position = getAdapterPosition();
                         if (position != RecyclerView.NO_POSITION) {
                             listener.OnItemClick(position);
+
                             final AlertDialog.Builder builder = new AlertDialog.Builder(view.getRootView().getContext());
                             @SuppressLint("ResourceType") View dialogView = LayoutInflater.from(view.getRootView().getContext()).inflate(R.layout.custom_dialog_for_add_amount, null);
                             de.hdodenhof.circleimageview.CircleImageView dialogImageView = dialogView.findViewById(R.id.dialog_boxImage);
                             builder.setView(dialogView);
                             builder.setCancelable(true);
+                            ///////////оптимизировать элементы алертдиалога
                             TextView dialog_boxUsername = dialogView.findViewById(R.id.dialog_boxUserName);
                             TextView dialog_boxPhone = dialogView.findViewById(R.id.dialog_boxUserPhone);
                             Button ContinueBtn = dialogView.findViewById(R.id.ContinueBtn);
@@ -170,29 +173,31 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.choosenConta
                             dialog_boxUsername.setText(ChoosenContacts.get(position).name);
                             dialog_boxPhone.setText(ChoosenContacts.get(position).number);
                             dialogImageView.setImageURI(imageUri);
+                            ///////////
                             final AlertDialog AmountDialog = builder.show();
                             final EditText AmountInput = dialogView.findViewById(R.id.dialog_UserAmountText);
+
                             Toast.makeText(view.getContext(), "Click!", Toast.LENGTH_SHORT).show();
                             ContinueBtn.setOnClickListener(new View.OnClickListener() {
                                 @Override
                                 public void onClick(View v) {
-                                    if(!AmountInput.getText().toString().equals(null)) {
-                                        AmountDialog.cancel();
+                                    if(!AmountInput.getText().toString().isEmpty()) {
+                                        try {
 
-                                        //AmountForPersonalityUser.setText(AmountInput.getText().toString());
-                                        //AmountForEveryUser.setText("   ");
+                                            String dif = AmountInput.getText().toString();
+                                            difVar = Integer.parseInt(dif);
+                                            newAmount = (int) (ChoosenContacts.get(position).amount - difVar);
+                                            String amI = String.valueOf(newAmount);
+                                            bar.setProgress(newAmount);
+                                            AmountForEveryUser.setText(amI);
+                                            ChoosenContacts.get(position).amount = Double.parseDouble(amI);
 
-                                        String dif = AmountInput.getText().toString();
-                                        String dif2 = AmountForEveryUser.getText().toString();
-                                        int difVar2 = Integer.parseInt(dif2);
-                                        difVar = Integer.parseInt(dif);
-                                        newAmount = (difVar2 - difVar);
-                                        String amI = String.valueOf(newAmount);
-                                        //AmountForEveryUserValue = Integer.parseInt(amI);
-                                        bar.setProgress(Integer.parseInt(amI));
-                                        AmountForEveryUser.setText(amI);
-                                        ChoosenContacts.get(position).amount = Double.parseDouble(amI);
-                                        AmountDialog.cancel();
+                                            AmountDialog.cancel();
+
+                                        }
+                                        catch (Exception e){
+                                            Log.v("Second on click :   ", "", e);
+                                        }
                                     }
                                     else{
                                         AmountDialog.cancel();
